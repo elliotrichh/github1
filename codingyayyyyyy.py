7b8d8add6d44b3fdec06acc66c5c82422002985f
elliotrichh <rollostudios@gmail.com>
2023-03-31 20:26:27 +0100

    for loops

--- codingyayyyyyy.py
+++ codingyayyyyyy.py
@@ -1,1 +1,12 @@
 #comment number1
+temp=0
+for i in range(10):
+    temp=temp+i
+    print("hi sage", i)
+    if i%2==0:
+        print("hi elliot", i)
+    if i%3==0:
+        print("cry elliot")
+    else:
+        print("hi aurora", i)
+print(temp)

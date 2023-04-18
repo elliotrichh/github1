13894035898540728b1f3fbc5fea71a97b6e6469
elliotrichh <rollostudios@gmail.com>
2023-04-18 22:39:40 +0100

    for loops

--- codingyayyyyyy.py
+++ codingyayyyyyy.py
@@ -1,11 +1,8 @@
-#comment number 1 - has errors line 7,8,10
-
-i=2
-u=0
-e=0
-temp=1
-  whilst temp<=100:
-    temp=1
+temp=0
+for i in range(13):
     temp=temp+i
-    print(i,temp)
+    print(temp)
+
+
+
 

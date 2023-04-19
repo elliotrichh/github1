47e3b854d7b1e0a130596110a9383719f89f6161
elliotrichh <rollostudios@gmail.com>
2023-04-19 18:48:04 +0100

    def main introduction

--- fibsequencepytho.py
+++ fibsequencepytho.py
@@ -1,12 +1,27 @@
-o=1
-t=2
-temp=0
-i=0
-for i in range(8):
-    if i%2==0:
-        temp=+temp
-    temp=o+t
-    o=t
-    t=temp
-    i=i+o
-    print (temp)
+def square(size):
+    i=0
+    for i in range(size):
+        print(i**2, "square")
+
+
+def fibseq(bounds):
+    o=1
+    t=2
+    temp=0
+    i=0
+    for i in range(bounds):
+        if i%2==0:
+            temp=+temp
+        temp=o+t
+        o=t
+        t=temp
+        i=i+o
+        print(temp, "fibseq")
+def main():
+    number=input("Enter a number for the Range")
+    number=int(number)
+    square(number)
+    fibseq(8)
+if __name__=='__main__':
+    main()
+

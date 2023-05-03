c4372c4fbe9b3746417d0b0b5898d7d95e70fd8d
elliotrichh <rollostudios@gmail.com>
2023-05-03 19:13:38 +0100

    fib seq sum of all odd + even hw -w errors

--- codingyayyyyyy.py
+++ codingyayyyyyy.py
@@ -1,10 +1,22 @@
-a=0
-b=1
-temp=0
-while b<100000:
-    if b%2==0:
-       temp+=b
-    a,b=b,a+b
-    print(temp)
 
+def even():
+    a=0
+    temp=0
+    while a<=100000:
+        if a%2==0:
+            temp+=a
+        a=a+1
+
+        print(temp)
+
+
+def odd():
+    b=0
+    tempA=0
+    while b<=100000:
+       if b%3==0:
+           tempA+=b
+    b=b+1
+
+    print(tempA)
 

ea0a049a1067f057d3ef86954497866356d7c43c
elliotrichh <rollostudios@gmail.com>
2023-05-03 19:30:57 +0100

    fib seq sum of all odd + even hw -fixed version

--- codingyayyyyyy.py
+++ codingyayyyyyy.py
@@ -2,23 +2,23 @@
 def even():
     a=0
     temp=0
-    while a<=10:
+    while a<=100000:
         if a%2==0:
             temp+=a
         a=a+1
 
-        print(temp, "even")
+    print("the sum of all even numbers to 10000 are",temp)
 
 
 def odd():
     b=0
     tempA=0
-    while b<=10:
-       if b%3==0:
+    while b<=100000:
+       if b%2!=0:
            tempA+=b
-    b=b+1
+       b=b+1
 
-    print(tempA, "oddd")
+    print("the sum of all odd numbers to 100000 are",tempA)
 
 if __name__ == '__main__':
     even()

1857db34d0e5052a63bcec370e272f2ff75e9958
elliotrichh <rollostudios@gmail.com>
2023-05-03 19:30:57 +0100

    triangle numbers + even fibonacci numbers
    -error on evenfib, output is constantly 0

--- codingyayyyyyy.py
+++ codingyayyyyyy.py
@@ -1,26 +1,29 @@
-
-def even():
-    a=0
+def triangle_numbers():
     temp=0
-    while a<=10:
-        if a%2==0:
-            temp+=a
+    for i in range(100000):
+        t=i*(i+1)//2
+        if t <100000:
+            temp+=t
+    print("the sum of all triangle numbers below 100k is", temp)
+
+def evenfib():
+    a=1
+    b=2
+    temp=0
+    while b<=1:
+        if b%2==0:
+            temp+=b
         a=a+1
+    print("the sum of all even fibonacci numbers below 100k is", temp)
 
-        print(temp, "even")
 
 
-def odd():
-    b=0
-    tempA=0
-    while b<=10:
-       if b%3==0:
-           tempA+=b
-    b=b+1
 
-    print(tempA, "oddd")
 
-if __name__ == '__main__':
-    even()
-    odd()
 
+def main():
+    triangle_numbers()
+    evenfib()
+
+if __name__=="__main__":
+    main()

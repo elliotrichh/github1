0a01a03d93ece93729f23bd2257b3d4f4e84be7c
elliotrichh <rollostudios@gmail.com>
2023-08-11 15:58:36 +0100

    GUI TIMEZONE PROJECT
       -added my dads time (pacific)
       -no comments yet as i am unable to make the code work

--- Gooey.py
+++ Gooey.py
@@ -1,9 +1,10 @@
 import tkinter as tk
 
+
 def calculate_time():
     elltime = int(entry.get())
     stime = (elltime - 5 + 24) % 24
-
+    dadstime = (elltime - 8 + 24) % 24
     if stime >= 12:
         am_pm = "PM"
     else:
@@ -13,12 +14,23 @@ def calculate_time():
     if stime == 0:
         stime = 12
 
-    resultoutput.config(text=f"{stime} USA Eastern {am_pm}")
+
+    if dadstime >= 12:
+        dads_am_pm = "PM"
+    else:
+        dads_am_pm = "AM"
+
+    dadstime %= 12
+    if dadstime == 0:
+        dadstime = 12
+
+    resultoutput.config(text=f"{stime} USA Eastern {am_pm}\n{dadstime} USA Pacific {dads_am_pm}")
+
 
 root = tk.Tk()
 root.title("Elliot's Timezone Converter")
 
-label = tk.Label(text="GMT to USA eastern:")
+label = tk.Label(text="GMT to USA Eastern and Pacific:")
 label.pack()
 
 entry = tk.Entry(root)
@@ -30,6 +42,5 @@ convertbutton.pack()
 resultoutput = tk.Label(root, text="")
 resultoutput.pack()
 
-root.mainloop()
 
 
